c01e16b390c364af01a9530c06b470d430278a2c
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-11-07 13:40:45 +0530

    nav bar added

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,32 @@
 import { AppBar, Toolbar } from "@mui/material";
 import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
 
 const Header = () => {
+
+  const auth = useAuth()
+
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
     >
       <Toolbar sx={{ display: "flex" }}>
         <Logo />
+        <div>
+        {
+          auth?.isLoggedIn?
+          (<>
+          <NavigationLink bg="#00fffc" to="/chat" text="Go To Chat" textColor="black" />
+          <NavigationLink bg="#51538f" to="/" text="logout" textColor="white" onClick={auth.logout} />
+          </>)
+          :
+          (<>
+          <NavigationLink bg="#00fffc" to="/login" text="login" textColor="black" />
+          <NavigationLink bg="#51538f" to="/signup" text="signup" textColor="white"/>
+          </>)
+        }
+        </div>
       </Toolbar>
     </AppBar>
   );
