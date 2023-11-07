65730fe2d271b292a73d1042d8c58aba3eda8d57
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-11-07 16:48:31 +0530

    login design completed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,28 @@
-import Header from "./components/Header"
-import { Routes,Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Signup from "./pages/Signup"
-import Login from "./pages/Login"
-import Chat from "./pages/Chat"
-import Notfound from "./pages/Notfound"
-import ParticlesBackground from "./components/ParticlesBackground"
-
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+import Notfound from "./pages/Notfound";
+import ParticlesBackground from "./components/ParticlesBackground";
 
 function App() {
-
-
-   
-  
-
   return (
-
     <main>
-      <ParticlesBackground/>
-      <Header/>
+      <div className="particles-container">
+        <ParticlesBackground />
+      </div>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/chat" element={<Chat/>} />
-        <Route path="/*" element={<Notfound/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/*" element={<Notfound />} />
       </Routes>
-      
     </main>
-
-  )
+  );
 }
 
-export default App
+export default App;
